e911fbc3d138bc19c89d0425c7501a3fa8001f91
Ahmed <ahmahm@hotmail.co.jp>
2018-09-04 17:21:09 +0200

    modifs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
-before_action :set_user
+	before_action :set_user
 
 	def show 
 	end
 
-private
+	private
 
 	def set_user
 		@user = User.find(params[:id])
 
 	end
+
 end
