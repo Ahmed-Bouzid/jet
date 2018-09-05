f0f55cabee123eefe190b8eac5ff8db7eaa2697d
Ahmed <ahmahm@hotmail.co.jp>
2018-09-05 13:53:36 +0200

    mercredi 14h

---
+++ app/assets/javascripts/untitled.js
@@ -0,0 +1,11 @@
+//= require revolution.extension.parallax.min
+//= require revolution.extension.navigation.min
+//= require revolution.extension.slideanims.min
+//= require revolution.extension.layeranimation.min
+//= require jquery.form.min
+//= require jquery.themepunch.tools.min
+//= require jquery.themepunch.revolution.min
+//= require revolution.extension.video.min
+//= require additional-methods.min
+//= require smart-form
+//= require restaurant.custom
