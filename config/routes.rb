c3742398c370e70a01e293cacb9d5a6cf63466a4
Ahmed <ahmahm@hotmail.co.jp>
2018-09-13 09:49:15 +0200

    thom

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,10 @@ Rails.application.routes.draw do
 	resources :aircraft
 
 	post "/recherche-de-vol", to: "aircraft#search_results", as: :search_results
-	#get "/mon-vol/:aircraft_id/:departure_city/:arrival_city/:number_of_passengers", to: "aircraft#trip", as: :trip
 	get "/mon-vol/:id", to: "aircraft#trip", as: :trip
 	
 	get "/aircrafts", to: 'aircraft#index'
 	get "/aircraft/:id", to: 'aircraft#show'
 	get "/planes", to: 'planes#index'
-	#get "/paiement", to: 'charges#new'
 
 end
