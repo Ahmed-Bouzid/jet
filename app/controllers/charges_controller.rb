3ac0652d89b69bf8902483e619e92fa97b1a0808
Ahmed <ahmahm@hotmail.co.jp>
2018-09-14 16:52:07 +0200

    supp mail

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -7,27 +7,14 @@ Dotenv.load
 class ChargesController < ApplicationController
 
 
-
-  def send_mail(adresse)
-
-    gmail = Gmail.connect(ENV['ADRESSE'],ENV['MDP'])
-    email = gmail.compose do
-      to adresse
-      subject "Reservation de Jet privé"
-      body "Bonjour #{@user_name},
-
-      Vous venez de reserver un Jet privé avec Stratton.
-      Notre equipe prend en charge votre vol et vous enverra tres bientot un flight-brief.
-      
-      John Smith, General Manager"
-    end
-
-    email.deliver!
-
-  end
+  #permet d'envoyer des emails confirmant la reservation.
   
+
+
   def new 
   end
+
+  #paiement strip en euro
 
   def create
     aircraft = Aircraft.find(params[:aircraft_id])
@@ -35,12 +22,12 @@ class ChargesController < ApplicationController
     @arrival_city = params[:arrival_city]
     @number_of_passengers = params[:number_of_passengers]
     @current_user_id = current_user.id
-    @user_name = User.find(@current_user_id).first_name
+    @user_name = User.find(@current_user_id).last_name
     @user_email = User.find(@current_user_id).email
 
 
     @amount = aircraft.trip_cost_with_commission(@departure_city, @arrival_city, @number_of_passengers).to_i * 100
-    # Amount in cents
+
 
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail],
@@ -53,9 +40,6 @@ class ChargesController < ApplicationController
       :currency    => 'eur'
       )
 
-
-
-    send_mail(@user_email)
     redirect_to root_path, notice: "Votre paiement a bien ete pris en compte"
 
   rescue Stripe::CardError => e
@@ -63,7 +47,7 @@ class ChargesController < ApplicationController
     redirect_to aircraft_path
   end
 
-  
+
 
 end
 
